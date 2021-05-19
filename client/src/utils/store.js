c3d04fa324f4aad24b7a5d870a1fd6a1c4793080
sidonie <shelliednnguyen@gmail.com>
2021-05-19 15:38:21 -0700

    fixed syntax errors in mutations.js

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -5,12 +5,4 @@
 
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-export default createStore(reducer);
-=======
 // export default createStore(reducer);
->>>>>>> 1ccff4cb6b376eaeaf215d2311c13511a84a7ea7
-=======
-export default { createStore, reducer };
->>>>>>> feature/signup-user
