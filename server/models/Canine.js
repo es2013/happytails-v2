aabf7a2d0c4d23e6f153f256f7050fe16e3a0701
Toby <agentalliancela33@gmail.com>
2021-05-18 17:25:04 -0700

    canine_models

--- server/models/Canine.js
+++ server/models/Canine.js
@@ -1,26 +1,39 @@
-const { Schema } = require('mongoose');
-
+const IsInteger = require("es-abstract/2015/IsInteger");
+const { Schema } = require("mongoose");
 
 const canineSchema = new Schema({
-  owner: [
-    {
-      type: String,
-    },
-  ],
-  description: {
+  kennelId: {
+    type: INTEGER,
+    required: true,
+  },
+  canineDemeanor: {
     type: String,
     required: true,
   },
-  
   canineId: {
-    type: String,
+    type: INTEGER,
     required: true,
   },
   image: {
     type: String,
   },
- 
   name: {
+    type: String,
+    required: true,
+  },
+  has_potty_am: {
+    type: String,
+    required: true,
+  },
+  has_potty_pm: {
+    type: String,
+    required: true,
+  },
+  has_walked_am: {
+    type: String,
+    required: true,
+  },
+  has_walked_pm: {
     type: String,
     required: true,
   },
