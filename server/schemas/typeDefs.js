1e9d676fea2cd8b685bc9cf893882465b12b0063
sidonie <shelliednnguyen@gmail.com>
2021-05-19 11:52:53 -0700

    Add new user in Playground

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,22 +7,22 @@ const typeDefs = gql`
     username: String
     email: String
     firstName: String
-    lastName: string
-  };
+    lastName: String
+  }
 
   type Query {
     me: User
-  };
+  }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!, firstName: String!, lastName: String!): Auth
-  };
+  }
 
   type Auth {
     token: ID!
     user: User
-  };
+  }
 `;
 
 // export the typeDefs
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// import ApolloServer
+
+// Import the ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-// import our typeDefs and resolvers
+// Import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-
+const { authMiddleware } = require('./utils/auth');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -26,7 +26,8 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+};
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
