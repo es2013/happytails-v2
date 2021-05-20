7be17bc92d45ab6927ad751d07068d36eda5d915
sidonie <shelliednnguyen@gmail.com>
2021-05-19 21:26:06 -0700

    gitignore fix

--- server/models/Canine.js
+++ server/models/Canine.js
@@ -1,26 +1,42 @@
 const { Schema } = require('mongoose');
 
 const canineSchema = new Schema({
-  owner: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  canineId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
   name: {
     type: String,
     required: true,
   },
+  kennel: {
+    type: String,
+    required: true,
+  },
+  demeanor: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  walk: [
+    {
+      timestamp: {
+        type: Date,
+      },
+      volunteer_id: {
+        type: String,
+      },
+    },
+  ],
+  potty_break: [
+    {
+      timestamp: {
+        type: Date,
+      },
+      volunteer_id: {
+        type: String,
+      },
+    },
+  ],
 });
 
 module.exports = canineSchema;
