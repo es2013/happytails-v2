2cbf72ba9dd02cbd70f9850cc8a267c6e97558ce
Erika <erikasalcedo013@gmail.com>
2021-05-19 17:15:17 -0700

    saving edits

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,32 @@
+// import the gql tagged template function
 const { gql } = require('apollo-server-express');
+
 const typeDefs = gql`
-    
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    firstName: String
+    lastName: String
+    isAdmin: Boolean
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!,
+            email: String!,
+            password: String!,
+            firstName: String!,
+            lastName: String!,
+            isAdmin: Boolean): Auth
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
-module.exports = typeDefs
+// export the typeDefs
+module.exports = typeDefs;
