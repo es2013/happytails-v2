4c8a4a5f559e84346fd01db649e4ad0f667381d1
sidonie <shelliednnguyen@gmail.com>
2021-05-20 08:40:06 -0700

    changed port number to test heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // create a new Apollo server and pass in our schema data
