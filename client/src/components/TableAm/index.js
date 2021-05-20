9255c815f23d6bf3da2e9ffec20030387e3cfe5d
Erika <erikasalcedo013@gmail.com>
2021-05-20 12:28:32 -0700

    Merge branch 'develop' of github.com:es2013/happytails-v2 into develop

--- client/src/components/TableAm/index.js
+++ client/src/components/TableAm/index.js
@@ -5,25 +5,28 @@ function TableAm() {
 
     return (
         // REACT reaallly doesnt like this segment and I am not sure why.
-        // <img className="dog-logo" src="/images/bulldog-favicon.png" alt="bulldog waiting to walk"></img>
+        <div className="table-container">
 
-        // <h4 className="flow-text">AM Shift</h4>
-        <table className="striped z-depth-2">
-            <thead>
-                <tr>
-                    <th>Doggy Name</th>
-                    <th>AM Potty Break</th>
-                    <th>AM Walk</th>
-                    <th>Doggy Demeanor</th>
-                    <th>Kennel</th>
-                </tr>
-            </thead>
-            <tbody>
+            <h4 class="flow-text"><span><img className="dog-logo" src="/images/bulldog-favicon.png" alt="bulldog waiting to walk" /></span>AM Shift</h4>
 
-                <DogRow />
 
-            </tbody>
-        </table>
+            <table className="striped z-depth-2">
+                <thead>
+                    <tr>
+                        <th>Doggy Name</th>
+                        <th>AM Potty Break</th>
+                        <th>AM Walk</th>
+                        <th>Doggy Demeanor</th>
+                        <th>Kennel</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <DogRow />
+
+                </tbody>
+            </table>
+        </div>
     );
 };
 
