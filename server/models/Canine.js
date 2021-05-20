6952a53daf9683b872dd0eac09f4c72fcce9a161
Erika <erikasalcedo013@gmail.com>
2021-05-19 20:14:54 -0700

    added canine model to server models

--- server/models/Canine.js
+++ server/models/Canine.js
@@ -1,26 +1,40 @@
 const { Schema } = require('mongoose');
 
-const canineSchema = new Schema({
-  owner: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  canineId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
+const canineSchema = new Schema(
+  {
   name: {
     type: String,
     required: true,
   },
+  kennel: {
+    type: String,
+    required: true,
+  },
+  demeanor: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  walk:[{
+    timestamp:{
+    type: Date
+    },
+    volunteer_id:{
+      type:String
+    }
+  }],
+  potty_break:[{
+    timestamp:{
+      type: Date
+      },
+      volunteer_id:{
+        type:String
+      }
+  }]
 });
 
 module.exports = canineSchema;
+	
