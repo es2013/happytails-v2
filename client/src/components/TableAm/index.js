45213c37ea0c8e41f2a39fad771d8c27a36a6e46
sidonie <shelliednnguyen@gmail.com>
2021-05-23 03:38:46 -0700

    implemented logged in logged oand get dog query

--- client/src/components/TableAm/index.js
+++ client/src/components/TableAm/index.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import DogRow from '../DogRow';
 import dogImage from '../../images/bulldog-favicon-trans-bg.png';
-import "./stylesheet.css";
+import './stylesheet.css';
 
 function TableAm() {
+  return (
+    // REACT reaallly doesnt like this segment and I am not sure why.
+    <div className="row s8">
+      <h4 className="flow-text">
+        <span>
+          <img
+            className="dog-logo"
+            src={dogImage}
+            alt="bulldog waiting to walk"
+          />
+        </span>
+        AM Shift
+      </h4>
 
-    return (
-        // REACT reaallly doesnt like this segment and I am not sure why.
-        <div className="row s8">
-            <h4 className="flow-text"><span><img className="dog-logo" src={dogImage} alt="bulldog waiting to walk" /></span>AM Shift</h4>
+      <table className="striped z-depth-2">
+        <thead>
+          <tr>
+            <th>Doggy Name</th>
+            <th>AM Potty Break</th>
+            <th>AM Walk</th>
+            <th>Doggy Demeanor</th>
+            <th>Kennel</th>
+          </tr>
+        </thead>
+        <tbody>
+          <DogRow />
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-
-            <table className="striped z-depth-2">
-                <thead>
-                    <tr>
-                        <th>Doggy Name</th>
-                        <th>AM Potty Break</th>
-                        <th>AM Walk</th>
-                        <th>Doggy Demeanor</th>
-                        <th>Kennel</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    <DogRow />
-
-                </tbody>
-            </table>
-        </div>
-    );
-};
-
-
-export default TableAm
+export default TableAm;
