cea1e9702fb9d19332832de281036e7e9a9cb48c
alex <alexreveles@yahooo.com>
2021-05-24 21:21:58 -0700

    css styling to am/pm table

--- client/src/components/TableAm/index.js
+++ client/src/components/TableAm/index.js
@@ -4,14 +4,22 @@ import dogImage from '../../images/bulldog-favicon-trans-bg.png';
 import './stylesheet.css';
 
 function TableAm({ dogData }) {
-
   return (
     // REACT reaallly doesnt like this segment and I am not sure why.
     <div className="row s8">
       <h4 className="flow-text">
-        <span> <img className="dog-logo" src={dogImage} alt="bulldog waiting to walk" /> </span>
+        <span>
+          {' '}
+          <img
+            className="dog-logo"
+            src={dogImage}
+            alt="bulldog waiting to walk"
+          />{' '}
+        </span>
         AM Shift
       </h4>
+      <br />
+      <br />
 
       <table className="striped z-depth-2">
         <thead>
@@ -25,7 +33,7 @@ function TableAm({ dogData }) {
         </thead>
 
         <tbody>
-          <DogRow dogData={ dogData }/>
+          <DogRow dogData={dogData} />
         </tbody>
       </table>
     </div>
