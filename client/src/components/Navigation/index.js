357b219c158f1c42852c6939a0a37566ccb0217d
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-05-28 12:30:15 -0700

    using media queries to style nav

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -3,57 +3,49 @@ import { Link } from 'react-router-dom';
 import './stylesheet.css';
 import { useAuth } from '../../utils/GlobalState';
 
-const { JSDOM } = require("jsdom");
-const { window } = new JSDOM("");
-const $ = require("jquery")(window);
-
-// import menuIcon from '../../images/menu-icon.png'
-
 function Navigation() {
   const { isAdmin, token } = useAuth();
 
- const sidebar = () => {
-    $(document).ready(function () {
-      $('.sidenav').sidenav();
-    });
-  }
-  
   return (
     <header className="navbar-fixed">
       <nav>
-        <div className="nav-wrapper grey">
-          <a href="/" className="homepage-link">
-            Happy Tails
-          </a>
+        <div className="row nav-wrapper grey">
+          <div className="col s6">
+            <a href="/" className="homepage-link">
+              Happy Tails
+            </a>
+          </div>
 
-          {/* sidebar trigger */}
-          <a onClick={sidebar} href="#" data-target="mobile-view" className="sidenav-trigger"><i className="material-icons">MENU</i></a>
-          <ul id="nav-mobile" className="right">
-            {/* {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>} */}
+          <div className="col s6">
+            <ul id="nav-mobile" className="right">
+              {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
 
-            {/* {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>} */}
+              {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
 
-            {/* {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>} */}
+              {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
 
-            {!isAdmin && token && <li><Link to="/dashboard">Dashboard</Link></li>}
+              {!isAdmin && token && <li><Link to="/dashboard">Dashboard</Link></li>}
 
-            <li><Link to="/donate">Donate</Link></li>
+              <li><Link to="/donate">Donate</Link></li>
 
-            {token && <li><Link id="logout" to="/logout">Logout</Link></li>}
+              {token && <li><Link id="logout" to="/logout">Logout</Link></li>}
 
-            {!token && <li><Link to="/login">Login</Link></li>}
-          </ul>
+              {!token && <li><Link to="/login">Login</Link></li>}
+            </ul>
+          </div>
         </div>
       </nav>
 
+
+
       {/* mobile view */}
-      <ul className="sidenav" id="mobile-admin-nav">
+      {/* <ul className="sidenav" id="mobile-admin-nav">
         {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
 
         {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
 
         {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
-      </ul>
+      </ul> */}
 
     </header>
   );
