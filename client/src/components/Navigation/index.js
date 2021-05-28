cab53aaa9a433b7c3111d47625ebfb1b9d78b154
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-05-28 10:34:16 -0700

    materialize dropdown being a lil bitch.  Am going to try refactoring to React navbar

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -3,9 +3,21 @@ import { Link } from 'react-router-dom';
 import './stylesheet.css';
 import { useAuth } from '../../utils/GlobalState';
 
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
+
+// import menuIcon from '../../images/menu-icon.png'
+
 function Navigation() {
   const { isAdmin, token } = useAuth();
 
+ const sidebar = () => {
+    $(document).ready(function () {
+      $('.sidenav').sidenav();
+    });
+  }
+  
   return (
     <header className="navbar-fixed">
       <nav>
@@ -13,12 +25,15 @@ function Navigation() {
           <a href="/" className="homepage-link">
             Happy Tails
           </a>
+
+          {/* sidebar trigger */}
+          <a onClick={sidebar} href="#" data-target="mobile-view" className="sidenav-trigger"><i className="material-icons">MENU</i></a>
           <ul id="nav-mobile" className="right">
-            {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
+            {/* {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>} */}
 
-            {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
+            {/* {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>} */}
 
-            {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
+            {/* {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>} */}
 
             {!isAdmin && token && <li><Link to="/dashboard">Dashboard</Link></li>}
 
@@ -30,6 +45,16 @@ function Navigation() {
           </ul>
         </div>
       </nav>
+
+      {/* mobile view */}
+      <ul className="sidenav" id="mobile-admin-nav">
+        {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
+
+        {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
+
+        {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
+      </ul>
+
     </header>
   );
 }
