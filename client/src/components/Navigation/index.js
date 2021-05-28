5ce69378349c4ce9794a078934bb3eef67627126
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-05-28 16:38:53 -0700

    removed add-dog, see-users, and donate link from nav.  admin can access users and add dog in dashboard via buttons.  donate link can be in footer.

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -14,19 +14,21 @@ function Navigation() {
             <a href="/" className="homepage-link">
               Happy Tails
             </a>
-          </div>
+            {/* <a href="#" data-target="mobile-admin-view" class="sidenav-trigger"><i class="material-icons">menu</i></a> */}
+        </div>
 
           <div className="col s6">
+            {/* <ul id="nav-mobile" className="right hide-on-med-and-down"> */}
             <ul id="nav-mobile" className="right">
-              {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
+              {/* {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>} */}
 
-              {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
+              {/* {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>} */}
 
               {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
 
               {!isAdmin && token && <li><Link to="/dashboard">Dashboard</Link></li>}
 
-              <li><Link to="/donate">Donate</Link></li>
+              {/* <li><Link to="/donate">Donate</Link></li> */}
 
               {token && <li><Link id="logout" to="/logout">Logout</Link></li>}
 
@@ -36,16 +38,28 @@ function Navigation() {
         </div>
       </nav>
 
-
-
       {/* mobile view */}
       {/* <ul className="sidenav" id="mobile-admin-nav">
-        {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
+      {isAdmin && <li><Link to="/add-dog">Add a Dog</Link></li>}
 
-        {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
+      {isAdmin && <li><Link to="/view-users">Dog Lovers</Link></li>}
 
-        {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
+      {isAdmin && <li><Link to="/admin-dashboard">Dashboard</Link></li>}
+
+      {!isAdmin && token && <li><Link to="/dashboard">Dashboard</Link></li>}
+
+      <li><Link to="/donate">Donate</Link></li>
+
+      {token && <li><Link id="logout" to="/logout">Logout</Link></li>}
+
+      {!token && <li><Link to="/login">Login</Link></li>}
       </ul> */}
+
+      {/* <script>
+        {$(document).ready(function(){
+         $('.sidenav').sidenav()})}
+      </script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> */}
 
     </header>
   );
