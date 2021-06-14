b7c8d9173875f12b4b7a040177089d9ffb3ce601
Toby <agentalliancela33@gmail.com>
2021-06-14 11:12:25 -0700

    image deletions

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,7 +18,7 @@ ReactDOM.render(
 
 //reportWebVitals(console.log);
 
-serviceWorker.register();
+
 
 
 //export default register();
