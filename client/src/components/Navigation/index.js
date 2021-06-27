93d7a63e2ad877f2eed0e914f75b02083e33d1b1
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-06-27 08:52:34 -0700

    added if else statement to see if that would help solve 's issue.  The issue still persists and now the username does not display.  deleted dowgrowomonecomponent

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -3,62 +3,70 @@ import { Link } from 'react-router-dom';
 import './stylesheet.css';
 import { useAuth } from '../../utils/GlobalState';
 
-function Navigation() {
+const DashboardLinks = () => {
   const { isAdmin, token, currentUsername } = useAuth();
 
-  function DashboardLink() {
-    if (!isAdmin && token) {
-      return (
-        <header className="navbar-fixed">
-          <nav>
-            <div className="row nav-wrapper grey">
-              <div className="col s6 ">
-                <a href="/" className="homepage-link">
-                  Happy Tails
-                </a>
-              </div>
-
-              <div className="col s6">
-                <ul id="nav-mobile" className="right">
-                  <li><Link to="/dashboard"><span className="username">{currentUsername}'s</span> Dashboard</Link></li>
-                  <li><Link id="logout" to="/logout">Logout</Link></li>
-                  <li><Link to="/login">Login</Link></li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </header>
-      )
-    }
+  if (!isAdmin && token) {
+    return (
+      <div className="col s6">
+        <ul id="nav-mobile" className="right">
+          <li><Link to="/dashboard"><span className="username">{currentUsername}'s</span> Dashboard</Link></li>
+          <li><Link id="logout" to="/logout">Logout</Link></li>
+        </ul>
+      </div>
+    )
   }
+  else if (isAdmin && token) {
+    return (
+      <div className="col s6">
+        <ul id="nav-mobile" className="right">
+          <li><Link to="/admin-dashboard"><span className="username">{currentUsername}'s</span> Dashboard</Link></li>
+          <li><Link id="logout" to="/logout">Logout</Link></li>
+        </ul>
+      </div>
+    )
+  }
+  else if (!token) {
+    return (
+      <div className="col s6">
+        <ul id="nav-mobile" className="right">
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </div>
+    )
+  }
+};
 
-  return (
-    <header className="navbar-fixed">
-      <nav>
-        <div className="row nav-wrapper grey">
-          <div className="col s6 ">
-            <a href="/" className="homepage-link">
-              Happy Tails
-            </a>
-            {/* <a href="#" data-target="mobile-admin-view" class="sidenav-trigger"><i class="material-icons">menu</i></a> */}
-          </div>
+  function Navigation() {
+    return (
+      <header className="navbar-fixed">
+        <nav>
+          <div className="row nav-wrapper grey">
+            <div className="col s6 ">
+              <a href="/" className="homepage-link">
+                Happy Tails
+              </a>
+              {/* <a href="#" data-target="mobile-admin-view" class="sidenav-trigger"><i class="material-icons">menu</i></a> */}
+            </div>
 
-          <div className="col s6">
-            <ul id="nav-mobile" className="right">
-              {/* if user is admin, but not logged in they will see the " 's Dashboard"  this may be the issue */}
-              {/* add && token to line makes no difference */}
-              {/* perhaps something to do with the way that span is set up? Tried using p tag and adding concatination within the JSX expression, but no change.*/}
-              {/* {isAdmin && <li><Link to="/admin-dashboard"><p className="username">{currentUsername + "'s Dashboard"}</p></Link></li>}  */}
-              {/* lets try an if-else statement in a function above the return */}
-
+            <div className="col s6">
+              <ul id="nav-mobile" className="right">
+                {/* if user is admin, but not logged in they will see the " 's Dashboard"  this may be the issue */}
+                {/* add && token to line makes no difference */}
+                {/* perhaps something to do with the way that span is set up? Tried using p tag and adding concatination within the JSX expression, but no change.*/}
+                {/* {isAdmin && <li><Link to="/admin-dashboard"><p className="username">{currentUsername + "'s Dashboard"}</p></Link></li>}  */}
+                {/* lets try an if-else statement in a function above the return */}
+                {DashboardLinks()}
+                {/* {isAdmin && <li><Link to="/admin-dashboard"><span className="username">{currentUsername}'s</span> Dashboard</Link></li>}
               {!isAdmin && token && <li><Link to="/dashboard"><span className="username">{currentUsername}'s</span> Dashboard</Link></li>}
               {token && <li><Link id="logout" to="/logout">Logout</Link></li>}
-              {!token && <li><Link to="/login">Login</Link></li>}
-            </ul>
+              {!token && <li><Link to="/login">Login</Link></li>} */}
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
-  );
-}
-export default Navigation;
+        </nav>
+      </header>
+    );
+  };
+
+  export default Navigation;
