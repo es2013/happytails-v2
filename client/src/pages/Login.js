e4d476f0b9f47813d1ee06b64d4fd836f332e8f4
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-06-29 17:11:20 -0700

    added Erika pic to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,7 @@ import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { useAuth } from '../utils/GlobalState';
 import { useHistory } from 'react-router-dom';
+import erikaAndFren from '../images/erika-and-fren.png';
 
 function Login() {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -61,59 +62,64 @@ function Login() {
   };
 
   return (
-    <div className="login-container container my-1">
-      <div className="content-wrap">
-        <Link className="login-signup-toggle" to="/signup">
-          ← Go to Signup
-        </Link>
+    <section>
+      <div className="login-container container my-1">
+        <div className="content-wrap">
+          <Link className="login-signup-toggle" to="/signup">
+            ← Go to Signup
+          </Link>
 
-        <h2>Login</h2>
+          <h2>Login</h2>
 
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+          {error ? (
+            <div>
+              <p className="error-text">The provided credentials are incorrect</p>
+            </div>
+          ) : null}
+
+          <div className="row">
+            <form onSubmit={handleFormSubmit}>
+              <div className="flex-row space-between my-2">
+                <label className="email input-title-primary" htmlFor="email">
+                  Email address:
+                </label>
+                <input
+                  className="input"
+                  placeholder="youremail@test.com"
+                  name="email"
+                  type="email"
+                  id="email"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row space-between my-2">
+                <label className="password input-title-secondary" htmlFor="pwd">
+                  Password:
+                </label>
+                <input
+                  className="input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  id="pwd"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-row flex-end">
+                <button className="btn" type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
-        ) : null}
-
-        <div className="row">
-          <form onSubmit={handleFormSubmit}>
-            <div className="flex-row space-between my-2">
-              <label className="email input-title-primary" htmlFor="email">
-                Email address:
-              </label>
-              <input
-                className="input"
-                placeholder="youremail@test.com"
-                name="email"
-                type="email"
-                id="email"
-                onChange={handleChange}
-              />
+          <div className="row">
+            <div className="col s12 m center">
+              <img className="happy-doggo image-border" src={erikaAndFren}></img>
             </div>
-            <div className="flex-row space-between my-2">
-              <label className="password input-title-secondary" htmlFor="pwd">
-                Password:
-              </label>
-              <input
-                className="input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="pwd"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="flex-row flex-end">
-              <button className="btn" type="submit">
-                Submit
-              </button>
-              <br />
-              <br />
-            </div>
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
